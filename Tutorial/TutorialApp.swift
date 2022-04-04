70f79ac3bb0fec8b724df1eb1916280f2e4d26ae
Chenyang Liu <ripfreeworld@icloud.com>
2022-04-04 20:59:09 +0200

    Initial Commit

---
+++ Tutorial/TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorialApp.swift
+//  Tutorial
+//
+//  Created by Chenyang Liu on 04.04.22.
+//
+
+import SwiftUI
+
+@main
+struct TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
